a43f18e42f3599b790d45bcc358b80cb7e2c7d6c
amar.singh <amar.singh@kuehne-nagel.com>
2022-10-12 14:33:55 +0300

    netlify redirect file

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,18 @@
-import "./cart-item.styles.scss";
+import { CartItemContainer, ItemDetails } from "./cart-item.styles";
 
 const CartItem = ({ item }) => {
   const { name, imageUrl, quantity, price } = item;
   console.log("asd" + item);
   return (
-    <div className="cart-item-container">
+    <CartItemContainer>
       <img src={imageUrl} alt={name} />
-      <div className="item-details">
+      <ItemDetails>
         <span className="name">{name}</span>
         <span className="price">
           {quantity} x ${price}
         </span>
-      </div>
-    </div>
+      </ItemDetails>
+    </CartItemContainer>
   );
 };
 
